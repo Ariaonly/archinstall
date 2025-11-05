269785387464e4728a017f4c4a075dcce5449bf5
Ariaonly <2632898965@qq.com>
2025-11-05 15:45:28 +0800

    修改了很多之前的疑问与困惑

--- 2.archbetter.sh
+++ 2.archbetter.sh
@@ -5,7 +5,6 @@ sudo pacman -S fcitx5 fcitx5-configtool fcitx5-rime
 sudo pacman -S rime-pinyin-simp
 
 #配置环境变量-----解决typora vscode 无法输入中文的问题
-###但是当前有一个潜在问题，就是当我修改完后会导致vscode无法输入且关闭打开后问题依旧
 mkdir -p ~/.config/environment.d
 cat > ~/.config/environment.d/fcitx5.conf <<EOF2
 GTK_IM_MODULE=fcitx
