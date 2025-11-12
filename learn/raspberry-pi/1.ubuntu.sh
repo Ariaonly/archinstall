3f10b8945bc058858ceef1ca2c1f1639e367d8cd
Ariaonly <2632898965@qq.com>
2025-11-12 20:08:47 +0800

    nothing

--- learn/raspberry-pi/1.ubuntu.sh
+++ learn/raspberry-pi/1.ubuntu.sh
@@ -6,5 +6,6 @@ sudo sed -i 's#http://ports.ubuntu.com/ubuntu-ports#https://mirrors.tuna.tsinghu
 # 或者换成阿里云的源
 sudo sed -i 's#http://ports.ubuntu.com/ubuntu-ports#https://mirrors.aliyun.com/ubuntu-ports#g' /etc/apt/sources.list.d/ubuntu.sources
 sudo apt update
+sudo apt upgrade
 
 # ubuntu-ports 是 ARM 架构（树莓派用的），不要用 ubuntu 镜像，否则会 404。
